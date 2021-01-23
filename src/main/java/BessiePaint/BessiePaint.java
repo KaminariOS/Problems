0873ca9ad59902d6bd61227b14cded2b38b5f399
Kaminari <chankocyo@gmail.com>
2021-01-23 08:35:29 +0800

    Refactoring and add some comment.

--- src/main/java/BessiePaint/BessiePaint.java
+++ src/main/java/BessiePaint/BessiePaint.java
@@ -1,8 +1,6 @@
 package BessiePaint;
 
 import java.io.*;
-import java.util.Arrays;
-import java.util.LinkedList;
 import java.util.Scanner;
 
 
@@ -17,40 +15,27 @@ public class BessiePaint {
         while (sc.hasNext()){
             int n = sc.nextInt();
             int q = sc.nextInt();
+            // The desire color for the fence.
             char[] desire = sc.next().toCharArray();
+            /*
+             * The dp array that starts from left end, in which leftCount[i] stands for the minimum strokes needed to paint the fence from 0 to i.
+             */
             int[] leftCount = new int[n + 1];
+            /*
+             * The dp array that starts from right end, in which rightCount[i] stands for the minimum strokes needed to paint the fence from n to i.
+             */
             int[] rightCount = new int[n + 1];
 
-            Istack istack = new Istack();
-            for (int i = 0; i < n; i++) {
-                int ordinal = desire[i] - 'A';
-                if (ordinal > istack.peek()){
-                    leftCount[i + 1] = leftCount[i] + 1;
-                    istack.push(ordinal);
-                    continue;
-                }
-                int temp = -1;
-                while (ordinal <= istack.peek()){
-                    temp = istack.pop();
-                }
-                istack.push(ordinal);
 
-                leftCount[i + 1] = leftCount[i] + (ordinal == temp? 0: 1);
+            MonotonicStack stack = new MonotonicStack();
+            // Scanning the fence from left to right
+            for (int i = 0; i < n; i++) {
+                leftCount[i + 1] = leftCount[i] + stack.push(desire[i]);
             }
-            istack.clear();
+            stack.clear();
+            // Scanning the fence from right to left.
             for (int i = n - 1; i >= 0; i--) {
-                int ordinal = desire[i] - 'A';
-                if (ordinal > istack.peek()){
-                    rightCount[i] = rightCount[i + 1] + 1;
-                    istack.push(ordinal);
-                    continue;
-                }
-                int temp = -1;
-                while (ordinal <= istack.peek()){
-                    temp = istack.pop();
-                }
-                istack.push(ordinal);
-                rightCount[i] = rightCount[i + 1] + (ordinal == temp? 0: 1);
+                rightCount[i] = rightCount[i + 1] + stack.push(desire[i]);
             }
             BufferedWriter bufferedWriter = new BufferedWriter(new PrintWriter(System.out));
             for (int i = 0; i < q; i++) {
@@ -73,10 +58,13 @@ public class BessiePaint {
         return leftSum + rightSum;
     }
 
-    private static class Istack{
+    /**
+     * A simple array-based monotonic stack with a sentinel -1.
+     */
+    private static class MonotonicStack {
         int[] stack = new int[COUNT + 1];
         int sp = 0;
-        public Istack(){
+        public MonotonicStack(){
             stack[0] = -1;
         }
         public int peek(){
@@ -84,13 +72,25 @@ public class BessiePaint {
         }
 
         public void push(int ordinal) {
-            sp++;
-            stack[sp] = ordinal;
+            stack[++sp] = ordinal;
+        }
+        // Method overload.
+        public int push(char color) {
+            // Covert the char to int.
+            int ordinal = color - 'A';
+            // Here temp can be any integer as long as temp < 0 and temp >= 26. Set it to -1 Just for initialization.
+            int temp = -1;
+            // Keep the elements in the stack orderly(monotone increasing), which makes it a monotonic stack.
+            while (ordinal <= peek()){
+                temp = pop();
+            }
+            push(ordinal);
+            // If ordinal color is already in the stack, we don' t need to paint it again.
+            return temp == ordinal? 0:1;
         }
 
         public int pop() {
-            int i = stack[sp];
-            sp--;
+            int i = stack[sp--];
             return i;
         }
         public void clear(){
